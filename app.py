a675891181483ddf6f8211372ae1e9cd0f70736a
Marin Leontenko <marin.leontenko@gmail.com>
2021-09-01 15:19:28 +0200

    sentiment analysis for every review (nltk vader), add compound score to data

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import json
 from getcredits import get_credits
 from getgenres import get_genres
 from getreviews import get_reviews
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# sentiment analysis
+sid = SentimentIntensityAnalyzer()
 
 response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=67a6e1aeb9867e7d066045334c59e0a9&language=en-US&page=1")
 
@@ -21,6 +27,10 @@ for movie_fetched in movies_fetched["results"]:
     movie['cast'] = allcredits["cast"]
     movie['genres'] = get_genres(movie['moviedb_id'])
     movie['reviews'] = get_reviews(movie['moviedb_id'])
+    movie['scores'] = []
+    for review in movie['reviews']:
+        score = sid.polarity_scores(review)
+        movie['scores'].append(score["compound"])        
     movies.append(movie) 
 
 print(movies)
