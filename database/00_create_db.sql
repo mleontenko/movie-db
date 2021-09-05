39b513b831508ea5ef95262c2ee793f8482cf17b
Marin Leontenko <marin.leontenko@gmail.com>
2021-09-05 13:15:12 +0200

    create db script

---
+++ database/00_create_db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE moviedb
+    WITH 
+    OWNER = root
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
